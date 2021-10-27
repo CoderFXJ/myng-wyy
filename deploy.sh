867ed4cdf713f192ccc028622103a305c1d3b158
GiteeFXJ <3010099292@qq.com>
2021-10-27 09:09:44 +0800

    完成首页轮播图模块

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+
+
+
+git add .
+echo -e "\n=========所有更改提交至暂存区完毕！=========\n"
+
+git commit -m '完成首页轮播图模块'
+
+echo -e "\n========暂存区内容添加到本地仓库完毕！=========\n"
+
+
+git push -f git@github.com:CodingFXJ/myng-wyy.git 
+
+echo -e "\n=========博客源码推送完毕！=========\n"
+ 
